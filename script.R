d8453516aa8dd6cb82cb44d1c792618c95a55426
Daniel Durling <danieldurling@gmail.com>
2021-08-04 15:59:40 +0100

    Added script.R file

---
+++ script.R
@@ -0,0 +1,15 @@
+## code run during this exercise
+
+library(postcards)
+library(usethis)
+
+## create .Rmd file
+
+create_postcard(template = "jolla")
+
+# download a .jpeg of your face
+# edit the index.Rmd file
+
+## use the repo to create a github pages site
+
+use_github_pages(branch = "main", path = "/")
